b5a81ab6689755e4171de0a1bd61d520e4679c27
gpap <gpap@brainsum.com>
2016-10-07 17:57:03 +0200

    Load vendor scripts from CDNs, so they can be used as a dependency in .libraries.yml

---
+++ js/header.js
@@ -0,0 +1,19 @@
+/**
+ * @file
+ * JavaScript for paragraph type Header.
+ */
+(function (inlineVideo, Rellax, drupalSettings) {
+  'use strict';
+
+  // Allow autoplaying videos on mobile.
+  // @todo use hook_page_attachments().
+  if ('background is video according to drupalSettings') {
+    var video = document.querySelector('.background-video');
+    inlineVideo(video, !video.hasAttribute('muted'));
+  }
+
+  else if ('background is image according to drupalSettings') {
+    var parallax = new Rellax('.parallax');
+  }
+
+})(inlineVideo, Rellax, drupalSettings)
