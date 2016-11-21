ef257ed8edd95644942fe7ccd3768098b83673e1
Gergely Pap <gpap@brainsum.com>
2016-11-21 14:17:19 +0100

    Fixed and tested parallax and static backgrounds with image and video in Header paragraph type

--- js/parallax.js
+++ js/parallax.js
@@ -6,17 +6,6 @@
 (function ($) {
   'use strict';
 
-  // https://github.com/markdalgleish/stellar.js#configuring-everything
-  // .paragraph--type--parallax
-  // $.ready(function() {
-  //   $.stellar({
-  //     scrollProperty: 'transform',
-  //     positionProperty: 'transform',
-  //     parallaxElements: false,
-  //     horizontalScrolling: false,
-  //   });
-  // });
-
   var rellax = new Rellax('.bg-parallax', { speed: -4 });
 
 })(jQuery);
