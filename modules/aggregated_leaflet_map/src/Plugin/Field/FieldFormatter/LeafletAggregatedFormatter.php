68240d7a1bfcf533f068023fd3365cc018716a58
mhavelant <mhavelant@brainsum.com>
2017-03-14 17:36:22 +0100

    Upgraded the aggregated leaflet map formatter to get values from a user-set source field as popup info.

--- modules/aggregated_leaflet_map/src/Plugin/Field/FieldFormatter/LeafletAggregatedFormatter.php
+++ modules/aggregated_leaflet_map/src/Plugin/Field/FieldFormatter/LeafletAggregatedFormatter.php
@@ -3,6 +3,7 @@
 namespace Drupal\aggregated_leaflet_map\Plugin\Field\FieldFormatter;
 
 use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\leaflet\Plugin\Field\FieldFormatter\LeafletDefaultFormatter;
 
 /**
@@ -20,6 +21,42 @@ class LeafletAggregatedFormatter extends LeafletDefaultFormatter {
 
   /**
    * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return [
+      'source_field' => '',
+    ] + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $formState) {
+    $elements = parent::settingsForm($form, $formState);
+
+    $bundleFields = $this->getBundleFieldDefinitions($form['#entity_type'], $form['#bundle']);
+    $options = [];
+    foreach ($form['#fields'] as $field) {
+      if (!isset($bundleFields[$field])) {
+        continue;
+      }
+
+      $options[$field] = $bundleFields[$field]->getLabel();
+    }
+
+    $elements['source_field'] = [
+      '#title' => $this->t('Source field'),
+      '#type' => 'select',
+      '#options' => $options,
+      '#default_value' => $this->getSetting('source_field'),
+      '#required' => TRUE,
+    ];
+
+    return $elements;
+  }
+
+  /**
+   * {@inheritdoc}
    *
    * This function is called from parent::view().
    */
@@ -32,17 +69,26 @@ class LeafletAggregatedFormatter extends LeafletDefaultFormatter {
     $map['settings']['minZoom'] = isset($settings['minZoom']) ? $settings['minZoom'] : NULL;
     $map['settings']['maxZoom'] = isset($settings['zoom']) ? $settings['maxZoom'] : NULL;
 
-    $elements = array();
     // We collect all features in a single array.
     $aggregated_features = array();
-    foreach ($items as $delta => $item) {
+    // Try to get the source_field from the entity.
+    /** @var \Drupal\Core\Field\FieldItemList $sourceField */
+    if ($items->getEntity()->hasField($settings['source_field'])) {
+      $sourceField = $items->getEntity()->get($settings['source_field']);
+    }
 
+    foreach ($items as $delta => $item) {
       $features = leaflet_process_geofield($item->value);
 
-      // If only a single feature, set the popup content to the entity title.
-      if ($settings['popup'] && count($items) == 1) {
-        $features[0]['popup'] = $items->getEntity()->label();
+      // If the sourceField is not found, provide a fallback popup.
+      if (NULL === $sourceField) {
+        $features[0]['popup'] = $this->t('Location @delta', ['@delta' => $delta]);
       }
+      // Otherwise, get the value at $delta from the source field.
+      else {
+        $features[0]['popup'] = $sourceField->getValue()[$delta]['value'];
+      }
+
       if (!empty($icon_url)) {
         foreach ($features as $key => $feature) {
           $features[$key]['icon'] = $settings['icon'];
@@ -52,10 +98,23 @@ class LeafletAggregatedFormatter extends LeafletDefaultFormatter {
       $aggregated_features[] = $features;
     }
 
+    $elements = [];
     // We only display a single, aggregated map with every feature.
     $elements[0] = aggregated_leaflet_map_render_map($map, $aggregated_features, $settings['height'] . 'px');
 
     return $elements;
+  }
+
+  private function getBundleFieldDefinitions($entity, $bundle) {
+    /** @var \Drupal\Core\Entity\EntityFieldManagerInterface $tmp */
+    $fieldManager = \Drupal::service('entity_field.manager');
+    /** @var \Drupal\Core\Field\FieldDefinitionInterface[] $bundleFields */
+    $bundleFields = $fieldManager->getFieldDefinitions(
+      $entity,
+      $bundle
+    );
+
+    return $bundleFields;
   }
 
 }
