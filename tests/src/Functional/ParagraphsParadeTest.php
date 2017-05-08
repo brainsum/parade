5f5a9ba90b46aad7221817e53f87794b2ac65b80
odi <szanyitomi@gmail.com>
2017-05-08 17:19:08 +0200

    field_minimum_height to parade_minimum_height

--- tests/src/Functional/ParagraphsParadeTest.php
+++ tests/src/Functional/ParagraphsParadeTest.php
@@ -283,7 +283,7 @@ class ParagraphsParadeTest extends BrowserTestBase {
     $this->assertSession()->pageTextContains('Manage fields');
 
     $expected_labels = ['Anchor', 'Background', 'Background', 'Enable parallax', 'Minimum height', 'Text'];
-    $expected_machine_names = ['parade_anchor', 'parade_background', 'parade_image', 'parade_enable_parallax', 'field_minimum_height', 'parade_text'];
+    $expected_machine_names = ['parade_anchor', 'parade_background', 'parade_image', 'parade_enable_parallax', 'parade_minimum_height', 'parade_text'];
     $expected_types = ['Text (plain)', 'File', 'Image', 'Boolean', 'Number (integer)', 'Text (formatted, long)'];
 
     $this->ParagraphTypeFieldLabels($expected_labels);
