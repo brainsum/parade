0c651bcb0a222847f4eaf89f8bf62988ebb3b093
szato <szanyitomi@gmail.com>
2017-08-17 10:30:18 +0200

    Parade Demo settings - Parade Demo feature for content types.

---
+++ modules/parade_demo/src/Form/ParadeDemoSettingsForm.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace Drupal\parade_demo\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Symfony\Component\HttpFoundation\Request;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+use Drupal\field\Entity\FieldConfig;
+use Drupal\field\Entity\FieldStorageConfig;
+use Drupal\Core\Config\FileStorage;
+
+/**
+ * Defines a form that configures Parade settings.
+ */
+class ParadeDemoSettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'parade_demo_settings_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['parade_demo.settings'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, Request $request = NULL) {
+    $settings = $this->config('parade_demo.settings')->get('bundles');
+
+    $header = [
+      'bundle' => $this->t('Content type'),
+      'enabled' => $this->t('Parade feature'),
+//      'menu' => $this->t('Parade One-page menu'),
+    ];
+
+    $form['bundles'] = [
+      '#type' => 'table',
+      '#header' => $header,
+      '#empty' => $this->t('No content types available. <a href=":link">Add content type</a>.', [
+        ':link' => Url::fromRoute('node.type_add')
+          ->toString(),
+      ]),
+    ];
+
+    $contentTypes = \Drupal::service('entity.manager')
+      ->getStorage('node_type')
+      ->loadMultiple();
+    if (!empty($contentTypes)) {
+      foreach ($contentTypes as $contentType) {
+        $form['bundles'][$contentType->id()] = [
+          'bundle' => ['#markup' => $contentType->label()],
+          'enabled' => [
+            '#title' => $this->t('Enabled'),
+            '#type' => 'checkbox',
+            '#default_value' => isset($settings[$contentType->id()]) ? 1 : 0,
+          ],
+//          'menu' => [
+//            '#title' => 'Add menu field on Save',
+//            '#type' => 'checkbox',
+//            '#default_value' => (isset($settings[$contentType->id()]) && $settings[$contentType->id()]['menu']) ? 1 : 0,
+//          ],
+        ];
+      }
+      if (isset($form['bundles']['parade_onepage'])) {
+        $form['bundles']['parade_onepage']['enabled']['#value'] = 1;
+        $form['bundles']['parade_onepage']['enabled']['#disabled'] = TRUE;
+//        $form['bundles']['parade_onepage']['menu']['#value'] = 1;
+//        $form['bundles']['parade_onepage']['menu']['#disabled'] = TRUE;
+      }
+
+      $form['description'] = [
+        '#markup' => $this->t("For enabled content types on 'Save configuration' parade field will be added: parade_onepage_sections. Field should be deleted manually."),
+      ];
+    }
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * Add parade_ fields to bundle save enabled bundles to settings.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $enabled = $just_disabled = $just_enabled = [];
+    $settings = $this->config('parade_demo.settings')->get('bundles');
+    foreach ($form_state->getValue('bundles') as $bundle => $data) {
+      if ($data['enabled']) {
+        $enabled[$bundle] = $data;
+        unset($enabled[$bundle]['enabled']);
+      }
+      if ($data['enabled'] && (!isset($settings[$bundle]) || !$settings[$bundle]['enabled'])) {
+        $just_enabled[$bundle] = $data;
+      }
+      if (!$data['enabled'] && isset($settings[$bundle]) && $settings[$bundle]['enabled']) {
+        $disabled[$bundle] = $data;
+      }
+    }
+
+    if (!empty($just_enabled)) {
+      $configPath = drupal_get_path('module', 'parade_demo') . '/config/install';
+      $source = new FileStorage($configPath);
+
+      $field_names = [
+//        'parade_onepage_id',
+//        'parade_onepage_menu',
+        'parade_onepage_sections',
+      ];
+      foreach ($just_enabled as $bundle => $data) {
+
+        foreach ($field_names as $delta => $field_name) {
+          $field_storage = FieldStorageConfig::loadByName('node', $field_name);
+          $field = FieldConfig::loadByName('node', $bundle, $field_name);
+          if (empty($field)) {
+            // Load our base config data.
+            $config_name = 'field.field.node.parade_onepage.' . $field_name;
+            $config = $source->read($config_name);
+
+            $field = FieldConfig::create([
+              'field_storage' => $field_storage,
+              'bundle' => $bundle,
+              'label' => $config['label'],
+              'settings' => $config['settings'],
+            ]);
+            $field->save();
+
+            $config_name = 'core.entity_form_display.node.parade_onepage.default';
+            $config = $source->read($config_name);
+
+            // Assign widget settings for the 'default' form mode.
+            $entity_form_display = \Drupal::entityTypeManager()
+              ->getStorage('entity_form_display')
+              ->load('node.' . $bundle . '.default');
+            $title_weight = $entity_form_display->getComponent('title')['weight'];
+            $entity_form_display->setComponent($field_name, [
+              'type' => $config['content'][$field_name]['type'],
+              'settings' => $config['content'][$field_name]['settings'],
+              'region' => 'content',
+              'weight' => $title_weight + ($delta + 1),
+            ])->save();
+
+            $config_name = 'core.entity_view_display.node.parade_onepage.default';
+            $config = $source->read($config_name);
+
+            // Assign display settings for the 'default' view mode.
+            \Drupal::entityTypeManager()
+              ->getStorage('entity_view_display')
+              ->load('node.' . $bundle . '.default')
+              ->setComponent($field_name, [
+                'label' => $config['content'][$field_name]['label'],
+                'type' => $config['content'][$field_name]['type'],
+                'settings' => $config['content'][$field_name]['settings'],
+                'region' => 'content',
+                'weight' => $delta,
+              ])
+              ->save();
+          }
+        }
+      }
+    }
+
+    $this->config('parade_demo.settings')
+      ->set('bundles', $enabled)
+      ->save();
+
+    parent::submitForm($form, $form_state);
+  }
+
+}
