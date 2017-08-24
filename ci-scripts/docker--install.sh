5b346af78eeffe85f8a9b8880607c6ecc121fb7b
mhavelant <mhavelant@brainsum.com>
2017-08-24 12:05:38 +0200

    Fix for composer installing beta version instead of dev.

--- ci-scripts/docker--install.sh
+++ ci-scripts/docker--install.sh
@@ -4,7 +4,7 @@
 composer config repositories.leaflet '{"type": "package", "package": {"name": "leaflet/leaflet", "version": "v0.7.7", "type": "drupal-library", "dist": {"url": "https://github.com/Leaflet/Leaflet/archive/v0.7.7.zip", "type": "zip" }}}' \
   && composer config extra.patches-file "composer.patches.json" \
   && composer config discard-changes true \
-  && composer require drupal/parade drupal/coder \
+  && composer require drupal/parade:2.x-dev drupal/coder \
   && composer install -n \
   && vendor/bin/phpcs --config-set installed_paths /var/www/html/vendor/drupal/coder/coder_sniffer \
   && cd web \
