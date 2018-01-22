7bceef2b26640fe6ddb542d0a662096174e0c4d7
golddragon007 <mdanko@brainsum.com>
2018-01-22 17:23:23 +0100

    Patched in geysir module

--- modules/parade_edit/src/Controller/ParadeGeysirController.php
+++ modules/parade_edit/src/Controller/ParadeGeysirController.php
@@ -4,8 +4,6 @@ namespace Drupal\parade_edit\Controller;
 
 use Drupal\Core\Ajax\AjaxResponse;
 use Drupal\Core\Ajax\ReplaceCommand;
-use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\Ajax\HtmlCommand;
 use Drupal\Core\Entity\EntityFieldManager;
 use Drupal\geysir\Ajax\GeysirOpenModalDialogCommand;
 use Drupal\geysir\Controller\GeysirModalController;
@@ -70,70 +68,6 @@ class ParadeGeysirController extends GeysirModalController {
         $response->addCommand(new GeysirOpenModalDialogCommand($this->t('Add @paragraph_title', ['@paragraph_title' => $paragraph_title]), render($form), ["buttons" => []]));
       }
 
-      return $response;
-    }
-
-    return $this->t('Javascript is required for this functionality to work properly.');
-  }
-
-  /**
-   * Shift up a single paragraph.
-   */
-  public function up($parent_entity_type, $parent_entity_bundle, $parent_entity_revision, $field, $field_wrapper_id, $delta, $paragraph, $paragraph_revision, $js = 'nojs') {
-    if ($js == 'ajax') {
-      if ($delta > 0) {
-        /* @var $entity \Drupal\node\Entity\Node */
-        $entity = \Drupal::entityTypeManager()->getStorage('node')->loadRevision($parent_entity_revision);
-        $paragraphs = $entity->$field->getValue();
-        $paragraph = $paragraphs[$delta - 1];
-        $paragraphs[$delta - 1] = $paragraphs[$delta];
-        $paragraphs[$delta] = $paragraph;
-        $entity->$field->setValue($paragraphs);
-
-        // For creating new revision.
-        $entity->setRevisionLogMessage('Move up a paragraph.');
-
-        $entity->save();
-      }
-
-      $response = new AjaxResponse();
-      // Refresh the paragraphs field.
-      $response->addCommand(
-        new ReplaceCommand(
-          '[data-geysir-field-paragraph-field-wrapper=' . $field_wrapper_id . ']',
-          $entity->get($field)->view('default')));
-      return $response;
-    }
-
-    return $this->t('Javascript is required for this functionality to work properly.');
-  }
-
-  /**
-   * Shift down a single paragraph.
-   */
-  public function down($parent_entity_type, $parent_entity_bundle, $parent_entity_revision, $field, $field_wrapper_id, $delta, $paragraph, $paragraph_revision, $js = 'nojs') {
-    if ($js == 'ajax') {
-      /* @var $entity \Drupal\node\Entity\Node */
-      $entity = \Drupal::entityTypeManager()->getStorage('node')->loadRevision($parent_entity_revision);
-      $paragraphs = $entity->$field->getValue();
-      if ($delta < (count($paragraphs) - 1)) {
-        $paragraph = $paragraphs[$delta + 1];
-        $paragraphs[$delta + 1] = $paragraphs[$delta];
-        $paragraphs[$delta] = $paragraph;
-        $entity->$field->setValue($paragraphs);
-
-        // For creating new revision.
-        $entity->setRevisionLogMessage('Move down a paragraph.');
-
-        $entity->save();
-      }
-
-      $response = new AjaxResponse();
-      // Refresh the paragraphs field.
-      $response->addCommand(
-        new ReplaceCommand(
-          '[data-geysir-field-paragraph-field-wrapper=' . $field_wrapper_id . ']',
-          $entity->get($field)->view('default')));
       return $response;
     }
 
