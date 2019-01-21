af325e611719782d41585168599603909d14cc58
mhavelant <mhavelant@brainsum.com>
2019-01-21 15:46:55 +0100

    Refactor to use YML aliases, only install required packages.

--- .travis-ci/install-global-packages.sh
+++ .travis-ci/install-global-packages.sh
@@ -5,5 +5,4 @@
 composer global require drupal/coder:^8.3 \
     squizlabs/php_codesniffer:^3.0 \
     phpcompatibility/php-compatibility:^9.0 \
-    jakub-onderka/php-parallel-lint:^1.0 \
     dealerdirect/phpcodesniffer-composer-installer
